c33b2b85feb4be083f48ce311b5c522f3b0504c8
AnthonyPCPortable <anth06ny@gmail.com>
2026-09-10 14:02:47 +0200

    Tp maison

--- src/main/ExoHouse.java
+++ src/main/ExoHouse.java
@@ -5,10 +5,62 @@ import main.beans.HouseBean;
 public class ExoHouse {
 
     public static void main(String[] args) {
-        exo1();
+        //Exemple de test
+        HouseBean myHouse = new HouseBean();
+        myHouse.width = 12;
+        myHouse.length = 25;
+        printHouse(myHouse);
+        doubleHouse(myHouse);
+        printHouse(myHouse); //Normalement 24*50
+
+        HouseBean myHouse2 = new HouseBean();
+        myHouse2.width = 24;
+        myHouse2.length = 50;
+        HouseBean big = bigHouse(myHouse, myHouse2);
+        System.out.print("Plus grande maison : ");
+
+        printHouse(big);
     }
 
-    public static void exo1(){
+    //Affiche la largeur, longueur de la maison ou Null  : Exemple attendu :     12x25
+    public static void printHouse(HouseBean m) {
+        if(m == null) {
+            System.out.println("null");
+        }
+        else {
+            System.out.println(m.width + " " + m.length);
+        }
+    }
+
+    //Double la taille de la maison. (N'affiche rien)
+    public static void doubleHouse(HouseBean m) {
+        if(m != null) {
+            m.width = m.width * 2;
+            m.length *= 2;
+        }
+    }
+
+    //Retourne la maison la plus grande, et null si de la même taille. N'affiche rien
+    public static HouseBean bigHouse(HouseBean m1, HouseBean m2) {
+        if(m1 == null ) {
+                return m2;
+        }
+        else if(m2 == null ) {
+            return m1;
+        }
+        else if(m1.length * m1.width > m2.length * m2.width){
+            return m1;
+        }
+        else if(m1.length * m1.width < m2.length * m2.width){
+            return m2;
+        }
+        else {
+            return null;
+        }
+    }
+
+
+    public static void exo1() {
 //        Créer une variable m de type HouseBean initialisée avec null
         HouseBean m = null;
 
@@ -31,6 +83,6 @@ public class ExoHouse {
 //        Changer la largeur de m2 à 10
         m2.width = 10;
 //        Afficher la largeur de m et de m2
-        System.out.println(m.width + " "+ m2.width);
+        System.out.println(m.width + " " + m2.width);
     }
 }
